987cbfe40f26ea6ade9cc5a6c44f123beb06134e
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-11-28 22:32:56 -0500

    added name field and signup handler

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Signup.css";
+import { useSignupMutation } from "../services/appApi"
 
 function Signup() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [signup, { error, isLoading, isError }] = useSignupMutation();
+
+    function handleSignup(e) {
+        e.preventDefault();
+        signup({name, email, password})
+    }
+
   return (
     <Container>
             <Row>
                 <Col md={6} className="signup__form--container">
-                    <Form style={{ width: "100%" }} >
+                    <Form style={{ width: "100%" }} onSubmit={handleSignup}>
                         <h1>Create an account</h1>
+                        {isError && <Alert variant='danger'>{error.data}</Alert>}
+                        <Form.Group>
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Your name" value={name} required onChange={(e) => setName(e.target.value)}/>
+                        </Form.Group>
                         <Form.Group>
                             <Form.Label>Email Address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" value={email} required onChange={(e) => setEmail(e.target.value)}/>
@@ -23,8 +37,8 @@ function Signup() {
                         </Form.Group>
 
                         <Form.Group>
-                            <Button type="submit" >
-                                Login
+                            <Button type="submit" disabled={isLoading}>
+                                Create Account
                             </Button>
                         </Form.Group>
 
