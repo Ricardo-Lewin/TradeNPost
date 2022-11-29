0c5f71f8609b965f8f560737a44e9e35622ca50c
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-11-29 15:24:32 -0500

    added user logged in view

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import { useSelector } from 'react-redux'
 
 function App() {
+  const user = useSelector((state) => state.user);
   return (
     <div className="App">
       <BrowserRouter>
       <Navigation/>
       <Routes>
         <Route index element={<Home />} />
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/login' element={<Login/>}/>
+        {!user && (
+                        <>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/signup" element={<Signup />} />
+                        </>
+                    )}
         <Route path='*' element={<Home/>}/>
       </Routes>
       </BrowserRouter>
