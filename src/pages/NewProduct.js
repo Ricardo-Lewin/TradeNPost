058be243c24cf59b180edf9eae606638937d9b5c
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-07 18:07:29 -0500

    added handlesubmit and css

--- src/pages/NewProduct.js
+++ src/pages/NewProduct.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Alert, Col, Container, Form, Row, Button } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 import { useCreateProductMutation } from '../services/appApi';
+import axios from "../axios"
 import "./NewProduct.css"
 
 function NewProduct() {
@@ -14,12 +15,51 @@ function NewProduct() {
     const navigate = useNavigate();
     const [createProduct, {isError, error, isLoading, isSuccess}] = useCreateProductMutation();
 
+    function handleRemoveImg(imgObj) {
+        setImgToRemove(imgObj.public_id);
+        axios
+            .delete(`/images/${imgObj.public_id}/`)
+            .then((res) => {
+                setImgToRemove(null);
+                setImages((prev) => prev.filter((img) => img.public_id !== imgObj.public_id));
+            })
+            .catch((e) => console.log(e));
+    }
 
-  return (
-    <Container>
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!name || !description || !price || !category || !images.length) {
+            return alert("Please fill out all the fields");
+        }
+        createProduct({ name, description, price, category, images }).then(({ data }) => {
+            if (data.length > 0) {
+                setTimeout(() => {
+                    navigate("/");
+                }, 1500);
+            }
+        });
+    }
+
+    function showWidget() {
+        const widget = window.cloudinary.createUploadWidget(
+            {
+                cloudName: "dlygntleh",
+                uploadPreset: "hxfdia1g",
+            },
+            (error, result) => {
+                if (!error && result.event === "success") {
+                    setImages((prev) => [...prev, { url: result.info.url, public_id: result.info.public_id }]);
+                }
+            }
+        );
+        widget.open();
+    }
+
+    return (
+        <Container>
             <Row>
                 <Col md={6} className="new-product__form--container">
-                    <Form style={{ width: "100%" }} >
+                    <Form style={{ width: "100%" }} onSubmit={handleSubmit}>
                         <h1 className="mt-4">Create a product</h1>
                         {isSuccess && <Alert variant="success">Product created with succcess</Alert>}
                         {isError && <Alert variant="danger">{error.data}</Alert>}
@@ -49,6 +89,20 @@ function NewProduct() {
                                 <option value="phones">phones</option>
                                 <option value="laptops">laptops</option>
                             </Form.Select>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Button type="button" onClick={showWidget}>
+                                Upload Images
+                            </Button>
+                            <div className="images-preview-container">
+                                {images.map((image) => (
+                                    <div className="image-preview">
+                                        <img src={image.url} />
+                                        {imgToRemove != image.public_id && <i className="fa fa-times-circle" onClick={() => handleRemoveImg(image)}></i>}
+                                    </div>
+                                ))}
+                            </div>
                         </Form.Group>
 
                         <Form.Group>
