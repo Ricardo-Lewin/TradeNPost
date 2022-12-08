2a37895f9696300ab26e17f61c3f8bffd56ca76d
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-07 22:57:13 -0500

    typo fixes

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spinner } from "react-bootstrap";
+
 function Loading() {
     return (
         <div className="loading-container" style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
--- src/components/SimilarProduct.js
+++ src/components/SimilarProduct.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Badge, Card } from "react-bootstrap";
-import LinkContainer from "react-router-bootstrap/LinkContainer";
+import { LinkContainer } from "react-router-bootstrap";
 
 function SimilarProduct({ _id, name, category, pictures }) {
     return (
