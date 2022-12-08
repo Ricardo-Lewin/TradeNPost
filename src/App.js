82115083d5bd3f93b0c066d47aa19587b4275e76
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-08 14:53:17 -0500

    added scroll to top

--- src/App.js
+++ src/App.js
@@ -9,26 +9,28 @@ import NewProduct from './pages/NewProduct';
 import { useSelector } from 'react-redux'
 import ProductPage from './pages/ProductPage';
 import CategoryPage from './pages/CategoryPage';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   const user = useSelector((state) => state.user);
   return (
     <div className="App">
       <BrowserRouter>
-      <Navigation/>
-      <Routes>
-        <Route index element={<Home />} />
-        {!user && (
+        <ScrollToTop />
+        <Navigation/>
+        <Routes>
+          <Route index element={<Home />} />
+          {!user && (
                         <>
                             <Route path="/login" element={<Login />} />
                             <Route path="/signup" element={<Signup />} />
                         </>
                     )}
-        <Route path="/product/:id" element={<ProductPage />} />
-        <Route path="/category/:category" element={<CategoryPage />} />
-        <Route path="/new-product" element={<NewProduct />} />
-        <Route path='*' element={<Home/>}/>
-      </Routes>
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/category/:category" element={<CategoryPage />} />
+          <Route path="/new-product" element={<NewProduct />} />
+          <Route path='*' element={<Home/>}/>
+        </Routes>
       </BrowserRouter>
     </div>
   );
