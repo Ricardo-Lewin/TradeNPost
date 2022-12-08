670df10f327cf2df11ab5ec8822e5cc768b18684
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-07 21:58:32 -0500

    added Latest Products section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
-import { LinkContainer } from "react-router-bootstrap"
-import { Link } from 'react-router-dom'
-import categories from '../categories'
-import "./Home.css"
+import React, { useEffect } from 'react';
+import axios from '../axios';
+import { Col, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import categories from '../categories';
+import ProductPreview from '../components/ProductPreview';
+import { updateProducts } from '../features/productSlice';
+import './Home.css';
 
 function Home() {
+    const dispatch = useDispatch();
+    const products = useSelector(state => state.products);
+    const lastProducts = products.slice(0,8);
+    useEffect(() => {
+        axios.get("/products").then(({ data }) => dispatch(updateProducts(data)));
+    }, []);
+
   return (
     <div>
         <img src=" https://res.cloudinary.com/learn-code-10/image/upload/v1653947013/yqajnhqf7usk56zkwqi5.png" className="home-banner" />
         <div className="featured-products-container container mt-4">
             <h2>Last Products</h2>
             {/* last products here */}
+            <div className="d-flex justify-content-center flex-wrap">
+                    {lastProducts.map((product) => (
+                        <ProductPreview {...product} />
+                    ))}
+            </div>
         <div>
             <Link to="/category/all" style={{ textAlign: "right", display: "block", textDecoration: "none" }}>See more {">>"}</Link>
         </div>
