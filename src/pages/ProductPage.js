70494f399f662928d03c18aa8a5d86d99f949d56
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-07 22:19:11 -0500

    added similar products functionality and carousel

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import AliceCarousel from 'react-alice-carousel' //https://www.npmjs.com/package/react-alice-carousel
+import "react-alice-carousel/lib/alice-carousel.css";
 import { Col, Container, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import Loading from "../components/Loading";
 
 function ProductPage() {
     const { id } = useParams();
@@ -9,11 +12,40 @@ function ProductPage() {
     const [product, setProduct] = useState(null);
     const [similar, setSimilar] = useState(null);
 
+    useEffect(() => {
+        axios.get(`/products/${id}`).then(({ data }) => {
+            setProduct(data.product);
+            setSimilar(data.similar);
+        });
+    }, [id]);
+
+    if (!product) {
+        return <Loading />;
+    }
+    const responsive = {
+        0: { items: 1 },
+        568: { items: 2 },
+        1024: { items: 3 },
+    };
+
+    const handleDragStart = (e) => e.preventDefault();
+
+    const images = product.picture.map((picture) => <img className='product__carousel--image' src={picture.url} onDragStart={handleDragStart}/>)
+
+    let similarProducts = [];
+    if (similar) {
+        similarProducts = similar.map((product, idx) => (
+            <div className="item" data-value={idx}>
+                <SimilarProduct {...product} />
+            </div>
+        ));
+    }
 
   return (
     <Container className='pt-4' style={{position: 'relative'}}>
         <Row>
             <Col lg={6}>
+                <AliceCarousel mouseTracking items={images} controlsStrategy='alternate'/>
             </Col>
         </Row>
     </Container>
