ef14d750319755c011838c037efadb0a30a56b37
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-15 12:10:36 -0500

    added categories

--- src/categories.js
+++ src/categories.js
@@ -8,7 +8,15 @@ const categories = [
         img: "https://images.unsplash.com/photo-1464380573004-8ca85a08751a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGhvbmV8ZW58MHx8MHx3aGl0ZXw%3D&auto=format&fit=crop&w=800&q=60",
     },
 
-    { name: "Laptops", img: "https://images.unsplash.com/photo-1575909812264-6902b55846ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" }
+    { name: "Laptops", img: "https://images.unsplash.com/photo-1575909812264-6902b55846ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" 
+    },
+
+    { name: "Shoes", img: "https://res.cloudinary.com/dlygntleh/image/upload/v1671074389/samples/ecommerce/revolt-164_6wVEHfI-unsplash_ageldp.jpg" 
+    },
+
+    { name: "Clothes", img: "https://res.cloudinary.com/dlygntleh/image/upload/v1671074557/samples/ecommerce/angela-bailey-jlo7Bf4tUoY-unsplash_ky7ulg.jpg" },
+
+    { name: "Accessories", img: "https://res.cloudinary.com/dlygntleh/image/upload/v1671074950/samples/ecommerce/jocelyn-morales-wau1MohLj0Q-unsplash_ywfwax.jpg" },
 ]
 
 export default categories;
