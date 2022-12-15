ca25b63cf3a78d25a45509e168f678ff08160aae
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-15 12:13:38 -0500

    refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
-import axios from '../axios';
-import { Col, Row } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import categories from '../categories';
-import ProductPreview from '../components/ProductPreview';
-import { updateProducts } from '../features/productSlice';
-import './Home.css';
+import React, { useEffect } from "react";
+import axios from "../axios";
+import { v4 as uuid } from 'uuid';
+import { Col, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import categories from "../categories";
+import ProductPreview from "../components/ProductPreview";
+import { updateProducts } from "../features/productSlice";
+import "./Home.css";
 
 function Home() {
     const dispatch = useDispatch();
@@ -21,23 +22,23 @@ function Home() {
     <div>
         <img src="https://res.cloudinary.com/dlygntleh/image/upload/v1671070262/samples/ecommerce/vecteezy_online-shopping-on-phone-buy-sell-business-digital-web_4299835_akbx81.jpg" className="home-banner" />
         <div className="featured-products-container container mt-4">
-            <h2>Last Products</h2>
-            {/* last products here */}
+            <h2>Latest Products</h2>
+            {/* lat products here */}
             <div className="d-flex justify-content-center flex-wrap">
                     {lastProducts.map((product) => (
-                        <ProductPreview {...product} />
+                        <ProductPreview key={uuid()} {...product} />
                     ))}
             </div>
-        <div>
-            <Link to="/category/all" style={{ textAlign: "right", display: "block", textDecoration: "none" }}>See more {">>"}</Link>
-        </div>
         </div>
         
         <div className="recent-products-container container mt-4">
             <h2>Categories</h2>
+            <div>
+                <Link to="/category/all" style={{ textAlign: "right", display: "block", textDecoration: "none" }}>See all {">>"}</Link>
+            </div>
             <Row>
                 {categories.map((category) => (
-                    <LinkContainer to={`/category/${category.name.toLocaleLowerCase()}`}>
+                    <LinkContainer key={uuid()} to={`/category/${category.name.toLocaleLowerCase()}`}>
                     <Col md={4}>
                         <div style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${category.img})`, gap: "10px" }} className="category-tile">
                             {category.name}
